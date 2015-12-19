129ece859edb4014755306679273b2e39c542a36
HackingBuddha <algis999@yahoo.com>
2015-12-19 22:35:14 +0200

    Add more locations in the locations array

--- scripts/locations.js
+++ scripts/locations.js
@@ -1,7 +1,9 @@
-var locations = [];
-
-locations.push({title: 'Chicxulub Impact Crater', lat: 21.400, lng: -89.5167});
-locations.push({title: 'Wudang Mountains', lat: 32.4008, lng: 111.0039});
-locations.push({title: 'Okavango Delta', lat: -19.2500, lng: 22.75000});
-locations.push({title: 'The Great Barrier Reef', lat: -18.2861, lng: 147.7000});
-locations.push({title: 'Yellowstone Caldera', lat: 44.4000, lng: -110.7000});
+var locations = [
+	{title: 'Chicxulub Impact Crater', lat: 21.400, lng: -89.5167},
+	{title: 'Wudang Mountains', lat: 32.4008, lng: 111.0039},
+	{title: 'Okavango Delta', lat: -19.2500, lng: 22.75000},
+	{title: 'The Great Barrier Reef', lat: -18.2861, lng: 147.7000},
+	{title: 'Yellowstone Caldera', lat: 44.4000, lng: -110.7000},
+	{title: 'Nazca Lines', lat: -14.7167, lng: -75.1333},
+	{title: 'Dinosaur Provincial Park', lat: 50.7617, lng: -111.4850}
+];
