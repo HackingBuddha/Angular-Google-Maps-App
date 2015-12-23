bb7fc432a541706532c9eed9b3634bd176a537c5
HackingBuddha <algis999@yahoo.com>
2015-12-23 09:44:58 +0200

    Change loading of GMaps API to defer and add map.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,47 +1,12 @@
-
-var initMap  = function() {};
-
 // Setup and angular app
 var app = angular.module('app', []);
 
 // Setup and angular controller and location names to the $scope
-app.controller('SearchController', function($scope) {
+app.controller('mainController', function($scope) {
 
-	// For debugging
-	window.MY_SCOPE = $scope;
+	$scope.locations = [];
 
-	$scope.markers = [];
-	$scope.infoWindows = [];
-
-	initMap = function() {
-
-	  	// Create the new map
-	  	$scope.map = new google.maps.Map(document.getElementById('map'), {
-	    	center: {lat: 0, lng: 0},
-	    	zoom: 2
-	  	});
-  
-	  	// Create Markers
-		for (i = 0; i < locations.length; i++) {
-	    	$scope.markers[i] = new google.maps.Marker({
-	      		position: locations[i],
-	      		map: $scope.map,
-	      		title: locations[i].title,
-	    	});
-
-    		$scope.markers[i].index = i;
-
-		    // Create info windows
-		    $scope.infoWindows[i] = new google.maps.InfoWindow({
-		    	content: locations[i].title
-		    });
-
-		    // Create Info Window onClick listeners
-		    $scope.markers[i].addListener('click', function() {
-		    	$scope.infoWindows[this.index].open($scope.map, $scope.markers[this.index]);
-		    	$scope.map.panTo($scope.markers[this.index].getPosition());
-		    });
-  		}
+  	for (i = 0; i < markers.length; i++) {
+	    $scope.locations[i] = markers[i].title;
 	}
-
 });
---
+++ scripts/map.js
@@ -0,0 +1,34 @@
+var map;
+var markers = [];
+var infoWindows = [];
+
+initMap = function() {
+
+  // Create the new map
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: locations[0],
+    zoom: 2
+  });
+  
+  // Create Markers
+  for (i = 0; i < locations.length; i++) {
+    markers[i] = new google.maps.Marker({
+      position: locations[i],
+      map: map,
+      title: locations[i].title,
+    });
+
+    markers[i].index = i;
+
+    // Create info windows
+    infoWindows[i] = new google.maps.InfoWindow({
+      content: locations[i].title
+    });
+
+    // Create Info Window onClick listeners
+    markers[i].addListener('click', function() {
+      infoWindows[this.index].open(map, markers[this.index]);
+      map.panTo(markers[this.index].getPosition());
+    });
+  }
+}
